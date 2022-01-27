170cc59abc9983ba4575158a487efcbef307feac
rased100 <rased100@gmail.com>
2022-01-27 12:30:21 +0600

    footer

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,15 +1,84 @@
 import React from 'react';
+import {
+    Box,
+    Container,
+    Row,
+    Column,
+    FooterLink,
+    Heading,
+} from "./FooterStyles";
 
 const Footer = () => {
     return (
-        <div className='p-5 bg-secondary'>
-            <h4 className='p-5'>This is Footer</h4>
-            <h4 className='p-5'>This is Footer</h4>
-            <h4 className='p-5'>This is Footer</h4>
-            <h4 className='p-5'>This is Footer</h4>
-            <h4 className='p-5'>This is Footer</h4>
-            <h4 className='p-5'>This is Footer</h4>
-        </div>
+        <Box>
+            {/* <h1 style={{
+                color: "green",
+                textAlign: "center",
+                marginTop: "-50px"
+            }}>
+                Travel Agency
+            </h1> */}
+            <Container>
+                <Row>
+                    <Column>
+                        <Heading>About Us</Heading>
+                        <FooterLink href="#">Aim</FooterLink>
+                        <FooterLink href="#">Vision</FooterLink>
+                        <FooterLink href="#">Testimonials</FooterLink>
+                    </Column>
+                    <Column>
+                        <Heading>Services</Heading>
+                        <FooterLink href="#">Writing</FooterLink>
+                        <FooterLink href="#">Internships</FooterLink>
+                        <FooterLink href="#">Coding</FooterLink>
+                        <FooterLink href="#">Teaching</FooterLink>
+                    </Column>
+                    <Column>
+                        <Heading>Contact Us</Heading>
+                        <FooterLink href="#">Uttar Pradesh</FooterLink>
+                        <FooterLink href="#">Ahemdabad</FooterLink>
+                        <FooterLink href="#">Indore</FooterLink>
+                        <FooterLink href="#">Mumbai</FooterLink>
+                    </Column>
+                    <Column>
+                        <Heading>Social Media</Heading>
+                        <FooterLink href="#">
+                            <i className="fab fa-facebook-f">
+                                <span style={{ marginLeft: "10px" }}>
+                                    Facebook
+                                </span>
+                            </i>
+                        </FooterLink>
+                        <FooterLink href="#">
+                            <i className="fab fa-instagram">
+                                <span style={{ marginLeft: "10px" }}>
+                                    Instagram
+                                </span>
+                            </i>
+                        </FooterLink>
+                        <FooterLink href="#">
+                            <i className="fab fa-twitter">
+                                <span style={{ marginLeft: "10px" }}>
+                                    Twitter
+                                </span>
+                            </i>
+                        </FooterLink>
+                        <FooterLink href="#">
+                            <i className="fab fa-youtube">
+                                <span style={{ marginLeft: "10px" }}>
+                                    Youtube
+                                </span>
+                            </i>
+                        </FooterLink>
+                    </Column>
+                </Row>
+            </Container>
+            <span className='d-flex justify-content-evenly mt-5'>
+                <p className='text-white'>Privacy Policy</p>
+                <p className='text-white'>Copyright © 2022</p>
+            </span>
+
+        </Box>
     );
 };
 
